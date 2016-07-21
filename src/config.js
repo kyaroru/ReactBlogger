b2a33c4b37baaffacba343ca75811613a59edfeb
Carol <carol.chiew@ifastfinancial.com>
2016-07-21 16:27:51 +0800

    add config file

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// @flow
+export default {
+  domain: 'https://www.googleapis.com/blogger/v3/blogs/',
+  API_KEY:'AIzaSyC9nk0hBslBFGLeXWIJiw-k8yvAO5kuBCM',
+  client_id:'1082105685761-41cptdjlkebu8u73np4sssflv804mlsm.apps.googleusercontent.com',
+  client_secret:'OPx022Gx4M3zZfYClI0uXYU5',
+  auth:'Basic OPx022Gx4M3zZfYClI0uXYU5'
+}
